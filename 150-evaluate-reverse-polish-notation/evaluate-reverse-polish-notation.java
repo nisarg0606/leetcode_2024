7ad0e18ae6d34e3851fef13bf90b6b63fe2d561c
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2024-05-29 18:52:45 -0400

    Time: 6 ms (72.88%) | Memory: 44.6 MB (45.08%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -2,9 +2,6 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
         for (String token : tokens) {
-            // The intuition is to push the numbers to the stack and when we encounter an
-            // operator, we pop the last two numbers from the stack and perform the
-            // operation and push the result back to the stack.
             if (token.equals("+")) {
                 stack.push(stack.pop() + stack.pop());
             } else if (token.equals("-")) {
@@ -12,9 +9,9 @@ class Solution {
             } else if (token.equals("*")) {
                 stack.push(stack.pop() * stack.pop());
             } else if (token.equals("/")) {
-                int num1 = stack.pop();
-                int num2 = stack.pop();
-                stack.push(num2 / num1);
+                int n1 = stack.pop();
+                int n2 = stack.pop();
+                stack.push(n2 / n1);
             } else {
                 stack.push(Integer.parseInt(token));
             }
