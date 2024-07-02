f478de447f5660e16c1ea9c1f416a463b6432729
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2024-07-01 23:51:33 -0400

    Time: 7 ms (96.49%) | Memory: 56.8 MB (47.18%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        if (s == null || s.length() == 0) {
+            return new ArrayList<>();
+        }
+        List<List<String>> result = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+        backtrack(result, current, s, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, List<String> current, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                current.add(s.substring(start, end + 1));
+                backtrack(result, current, s, end + 1);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
