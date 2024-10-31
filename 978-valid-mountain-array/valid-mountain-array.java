b4019feebc8179f2d6bd039214ad30fe5f0daf83
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2024-10-30 20:30:06 -0400

    Time: 2 ms (79.75%) | Memory: 45.2 MB (61.80%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.java
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        if (arr.length < 3) {
+        int n = arr.length;
+        if (n < 3) {
             return false;
         }
         int maxIndex = 0;
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < n; i++) {
             if (arr[i] > arr[maxIndex]) {
                 maxIndex = i;
             }
         }
-        if (maxIndex == arr.length - 1 || maxIndex == 0) {
+        if (maxIndex == n - 1 || maxIndex == 0) {
             return false;
         }
         for (int i = maxIndex; i > 0; i--) {
