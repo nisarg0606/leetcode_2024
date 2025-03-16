3d233aeeb1e4b791a712e783c24360ca56cd3094
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-03-16 13:35:26 -0400

    Time: 13 ms (43.11%) | Memory: 44.4 MB (48.77%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,14 +2,17 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "");
         s = s.toLowerCase();
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+        // int left = 0, right = s.length() - 1;
+        // while (left < right) {
+        // if (s.charAt(left) != s.charAt(right)) {
+        // return false;
+        // }
+        // left++;
+        // right--;
+        // }
+        // return true;
+
+        String rev = new StringBuilder(s).reverse().toString();
+        return rev.equals(s);
     }
 }
