20a9fc033ba3cb914babb7f83cae0058c4e62cf7
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-03-16 21:19:39 -0400

    Time: 1 ms (100.00%) | Memory: 44.4 MB (71.91%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,9 +1,19 @@
 class Solution {
     public boolean divideArray(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i+=2){
-            if(nums[i] != nums[i+1]) return false;
-        }
+        // Arrays.sort(nums);
+        // for(int i = 0; i < nums.length; i+=2){
+        // if(nums[i] != nums[i+1]) return false;
+        // }
+        // return true;
+
+        int[] freq = new int[501];
+        for (int i : nums)
+            freq[i]++;
+
+        for (int count : freq)
+            if (count % 2 != 0)
+                return false;
+
         return true;
     }
 }
