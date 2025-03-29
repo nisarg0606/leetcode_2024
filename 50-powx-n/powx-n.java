8b2458f5c42cc8043e38782678b0d7f1da443681
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-03-29 18:44:54 -0400

    Time: 0 ms (100.00%) | Memory: 43.5 MB (10.41%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,15 +1,31 @@
 class Solution {
-    public double myPow(double x, int n) {
+    private double binaryExp(double x, long n) {
         if (n == 0) {
             return 1;
-        } 
-        else if (n < 0) {
-            return 1 / (x * myPow(x, -n - 1));
         }
-        else if (n%2==0){
-            double a = myPow(x,n/2);
-            return a*a;
+
+        // Handle case where, n < 0.
+        if (n < 0) {
+            n = -1 * n;
+            x = 1.0 / x;
         }
-        return x*myPow(x,n-1);       
+
+        // Perform Binary Exponentiation.
+        double result = 1;
+        while (n != 0) {
+            // If 'n' is odd we multiply result with 'x' and reduce 'n' by '1'.
+            if (n % 2 == 1) {
+                result = result * x;
+                n -= 1;
+            }
+            // We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
+            x = x * x;
+            n = n / 2;
+        }
+        return result;
+    }
+
+    public double myPow(double x, int n) {
+        return binaryExp(x, (long) n);
     }
 }
