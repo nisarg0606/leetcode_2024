f9cf8c460964442c5b3a704bf2e14d29db34bd72
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-04-08 16:33:23 -0400

    Time: 0 ms (100.00%) | Memory: 45.4 MB (98.55%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,22 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return dfs(0, 1, prices, dp);
-    }
-
-    public int dfs(int i, int buy, int[] prices, int[][] dp) {
-        if (i == prices.length) return 0;
-        if (dp[i][buy] != -1) return dp[i][buy];
-
-        if (buy == 1) {
-            int take = -prices[i] + dfs(i + 1, 0, prices, dp);
-            int notTake = dfs(i + 1, 1, prices, dp);
-            return dp[i][buy] = Math.max(take, notTake);
-        } else {
-            int sell = prices[i] + dfs(i + 1, 1, prices, dp);
-            int notSell = dfs(i + 1, 0, prices, dp);
-            return dp[i][buy] = Math.max(sell, notSell);
+        int profit = 0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1]; // Add profit from each increase
+            }
         }
+        
+        return profit;
     }
 }
