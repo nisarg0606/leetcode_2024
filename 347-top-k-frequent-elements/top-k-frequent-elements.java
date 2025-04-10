7194c888f74ceced73b20f807afb0bbe8da47914
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-04-09 21:45:56 -0400

    Time: 15 ms (40.46%) | Memory: 48.9 MB (43.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,27 +1,34 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
         }
-        // sort the map by value so that we can get the top k frequent elements
-        // we are using a priority queue to sort the map by value
-        // here the lambda function compares the values of the keys in the map and sorts
-        // them in ascending order
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> freq.get(a) - freq.get(b));
-
-        // add keys where the frequency is greater than or equal to k
-        for (int key : freq.keySet()) {
-            pq.add(key);
-            if (pq.size() > k) {
-                pq.poll();
-            }
+        
+        // 1. Build hash map: character and how often it appears
+        // O(N) time
+        Map<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+          count.put(n, count.getOrDefault(n, 0) + 1);
         }
 
-        int[] res = new int[k];
-        for (int i = k - 1; i >= 0; i--) {
-            res[i] = pq.poll();
+        // init heap 'the less frequent element first'
+        Queue<Integer> heap = new PriorityQueue<>(
+            (n1, n2) -> count.get(n1) - count.get(n2));
+
+        // 2. Keep k top frequent elements in the heap
+        // O(N log k) < O(N log N) time
+        for (int n: count.keySet()) {
+          heap.add(n);
+          if (heap.size() > k) heap.poll();    
         }
-        return res;
+
+        // 3. Build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            top[i] = heap.poll();
+        }
+        return top;
     }
 }
