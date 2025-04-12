b65d6d2aedd563bd6eeee6521e95ce2f5e8d8483
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-04-11 20:06:36 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (33.20%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,34 +1,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // iterate through the list to find the length
-        int len = 0;
-        ListNode curr = head;
-        while (curr != null) {
-            len++;
-            curr = curr.next;
+        ListNode tempList = head;
+        int length = 0;
+        while (tempList != null) {
+            length++;
+            tempList = tempList.next;
         }
-        // if the length is 1 and n is 1, return null
-        if (len == 1 && n == 1)
-            return null;
-        // if the length is equal to n, return the next node
-        if (len == n)
+
+        if (n == length) {
             return head.next;
-        // iterate through the list to find the node before the nth node from the end
-        curr = head;
-        for (int i = 0; i < len - n - 1; i++)
-            curr = curr.next;
-        // remove the nth node from the end
-        curr.next = curr.next.next;
+        }
+
+        int stepsToTake = length - n;
+        tempList = head;
+        for (int i = 1; i < stepsToTake; i++) {
+            tempList = tempList.next;
+        }
+
+        if (tempList.next != null) {
+            tempList.next = tempList.next.next;
+        }
+
         return head;
     }
 }
