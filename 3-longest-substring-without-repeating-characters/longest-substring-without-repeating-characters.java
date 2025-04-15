73f1e2efc763e13a749de6b3b0d4e78da9992ee3
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-04-15 00:47:09 -0400

    Time: 2 ms (98.51%) | Memory: 43.8 MB (87.00%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,17 +1,35 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int maxLen = 0;
+        // int maxLen = 0;
+        // int n = s.length();
+        // for (int i = 0; i < n; i++) {
+        //     int[] hash = new int[256];
+        //     for (int j = i; j < n; j++) {
+        //         if (hash[s.charAt(j)] == 1)
+        //             break;
+        //         int length = j - i + 1;
+        //         maxLen = Math.max(length, maxLen);
+        //         hash[s.charAt(j)] = 1;
+        //     }
+        // }
+        // return maxLen;
+
+        int[] hash = new int[256];
+        Arrays.fill(hash, -1);
+        int left = 0, right = 0, maxLen = 0;
         int n = s.length();
-        for (int i = 0; i < n; i++) {
-            int[] hash = new int[256];
-            for (int j = i; j < n; j++) {
-                if (hash[s.charAt(j)] == 1)
-                    break;
-                int length = j - i + 1;
-                maxLen = Math.max(length, maxLen);
-                hash[s.charAt(j)] = 1;
+        while (right < n) {
+            if (hash[s.charAt(right)] != -1) {
+                if (hash[s.charAt(right)] >= left)
+                    left = hash[s.charAt(right)] + 1;
             }
+            int length = right - left + 1;
+            maxLen = Math.max(length, maxLen);
+
+            hash[s.charAt(right)] = right;
+            right++;
         }
+
         return maxLen;
     }
 }
