728ec65bee1776b3f602c61385a7542842a797b4
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-04-19 12:45:42 -0400

    Time: 79 ms (27.51%) | Memory: 60.3 MB (66.52%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,14 +1,62 @@
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+//         int n = temperatures.length;
+//         int[] answer = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             int count = 1, flag = 0;
+//             for (int j = i + 1; j < n; j++) {
+//                 if (temperatures[i] < temperatures[j]) {
+//                     answer[i] = count;
+//                     flag = 1;
+//                     break;
+//                 } else {
+//                     count++;
+//                 }
+//             }
+//             if (flag == 0) {
+//                 answer[i] = 0;
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
+
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+//         int n = temperatures.length;
+//         int[] answer = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (temperatures[i] < temperatures[j]) {
+//                     answer[i] = j - i;
+//                     break;
+//                 }
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
         Stack<Integer> stack = new Stack<>();
-        int arr[] = new int[temperatures.length];
-        for (int i = 0; i < temperatures.length; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int index = stack.pop();
-                arr[index] = i - index;
+        int[] answer = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
             }
+
+            if (!stack.isEmpty()) {
+                answer[i] = stack.peek() - i;
+            }
+
             stack.push(i);
         }
-        return arr;
+
+        return answer;
     }
 }
