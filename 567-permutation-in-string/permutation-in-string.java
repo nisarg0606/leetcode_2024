a6e1028a2d88317e671ad2a06ac21c9eeb1c6f0f
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-04-21 12:03:46 -0400

    Time: 11 ms (44.14%) | Memory: 42.7 MB (69.74%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,16 +1,37 @@
 public class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        s1 = sort(s1);
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
-                return true;
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1arr = new int[26];
+        int[] s2arr = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1arr[s1.charAt(i) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']++;
         }
-        return false;
-    }
-    
-    public String sort(String s) {
-        char[] t = s.toCharArray();
-        Arrays.sort(t);
-        return new String(t);
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1arr[i] == s2arr[i])
+                count++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                return true;
+            s2arr[r]++;
+            if (s2arr[r] == s1arr[r]) {
+                count++;
+            } else if (s2arr[r] == s1arr[r] + 1) {
+                count--;
+            }
+            s2arr[l]--;
+            if (s2arr[l] == s1arr[l]) {
+                count++;
+            } else if (s2arr[l] == s1arr[l] - 1) {
+                count--;
+            }
+        }
+        return count == 26;
     }
 }
