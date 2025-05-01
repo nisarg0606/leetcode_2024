a9e230c1e14303660c37c9e7e53fccd5cd349fc8
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-05-01 02:00:49 -0400

    Time: 6 ms (70.48%) | Memory: 43.9 MB (87.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
         // int maxLen = 0;
         // int n = s.length();
         // for (int i = 0; i < n; i++) {
@@ -14,20 +14,41 @@ class Solution {
         // }
         // return maxLen;
 
-        int[] hash = new int[256];
-        Arrays.fill(hash, -1);
-        int left = 0, right = 0, maxLen = 0;
-        int n = s.length();
-        while (right < n) {
-            if (hash[s.charAt(right)] != -1) {
-                if (hash[s.charAt(right)] >= left)
-                    left = hash[s.charAt(right)] + 1;
-            }
-            int length = right - left + 1;
-            maxLen = Math.max(length, maxLen);
+        // int[] hash = new int[256];
+        // Arrays.fill(hash, -1);
+        // int left = 0, right = 0, maxLen = 0;
+        // int n = s.length();
+        // while (right < n) {
+        //     if (hash[s.charAt(right)] != -1) {
+        //         if (hash[s.charAt(right)] >= left)
+        //             left = hash[s.charAt(right)] + 1;
+        //     }
+        //     int length = right - left + 1;
+        //     maxLen = Math.max(length, maxLen);
 
-            hash[s.charAt(right)] = right;
-            right++;
+        //     hash[s.charAt(right)] = right;
+        //     right++;
+        // }
+
+        // return maxLen;
+    // }
+// }
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLen = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+
+            if (map.containsKey(ch) && map.get(ch) >= left) {
+                left = map.get(ch) + 1; // move left just after the previous occurrence
+            }
+
+            map.put(ch, right); // update latest index of current char
+            maxLen = Math.max(maxLen, right - left + 1); // update result
         }
 
         return maxLen;
