cac81edd5194a9911cd3d359498563cb7a719309
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-06-10 12:32:58 -0400

    Time: 20 ms (16.17%) | Memory: 48.6 MB (16.88%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,57 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String str : strs) {
-            char[] array = new char[26];
-            for (char c : str.toCharArray()) {
-                array[c - 'a']++;
+        /*
+        For the brute force approach I think I will take a hashmap where I will first 
+        take the first word and create a char array out of it. Will sort the character
+        array and make it the key. Now when the new words arrive will check by sorting
+        the characters
+        Eg:
+        "eat" -> "aet"
+        "tea" -> "aet"
+        'tan' -> 'ant'
+        'nat' -> 'ant'
+        'bat' -> 'abt'
+        
+        "aet" -> ["eat", "tea", "ate"]
+        "ant" -> ["tan", "nat"]
+        "abt" -> ["bat"]
+        */
+
+        //    Map<String, List<String>> ans = new HashMap<>();
+
+        //    for(String s: strs){
+        //     char[] chars = s.toCharArray();
+        //     Arrays.sort(chars);
+        //     String key = new String(chars);
+        //     if(!ans.containsKey(key)){
+        //         ans.put(key, new ArrayList<>());
+        //     }
+        //     ans.get(key).add(s);
+        //    }
+
+        //    return new ArrayList<>(ans.values());
+
+        Map<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
             }
-            String key = new String(array);
-            if (!map.containsKey(key)){
-                map.put(key, new ArrayList<>());
+
+            StringBuilder sb = new StringBuilder();
+            for (int num : count) {
+                sb.append(num).append("#");
             }
-            map.get(key).add(str);
+            String key = sb.toString();
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
         }
-        return new ArrayList<>(map.values());
+
+        return new ArrayList<>(ans.values());
+
     }
 }
