88536a4c94b6855eec859b22e7d68074e4ec73f7
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-06-15 18:29:20 -0400

    Time: 1 ms (87.59%) | Memory: 40.8 MB (39.42%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -1,46 +1,69 @@
 class Solution {
-
+    /**
+     * Calculate the maximum possible difference by applying two separate digit replacement operations
+     * @param num The input integer to be modified
+     * @return The maximum possible difference between the resulting numbers
+     */
     public int maxDiff(int num) {
-        StringBuffer min_num = new StringBuffer(String.valueOf(num));
-        StringBuffer max_num = new StringBuffer(String.valueOf(num));
+        // Convert num to string for easier digit manipulation
+        StringBuffer maxNum = new StringBuffer(String.valueOf(num));
+        StringBuffer minNum = new StringBuffer(String.valueOf(num));
+        int maxLen = maxNum.length();
 
-        // Find a high position and replace it with 9.
-        int max_length = max_num.length();
-        for (int i = 0; i < max_length; ++i) {
-            char digit = max_num.charAt(i);
+        // STRATEGY 1: Maximize the number by replacing the leftmost non-9 digit with 9
+        // We want to find the leftmost digit that isn't already 9 and replace all occurrences of it with 9
+        for (int i = 0; i < maxLen; i++) {
+            char digit = maxNum.charAt(i);
             if (digit != '9') {
-                replace(max_num, digit, '9');
-                break;
+                // Found the leftmost non-9 digit, replace all occurrences with 9
+                replace(maxNum, digit, '9');
+                break; // Only perform one replacement operation
             }
         }
 
-        // Replace the most significant bit with 1
-        // Or find a high-order digit that is not equal to the highest digit and replace it with 0.
-        int min_length = min_num.length();
-        for (int i = 0; i < min_length; ++i) {
-            char digit = min_num.charAt(i);
+        int minLen = minNum.length();
+        
+        // STRATEGY 2: Minimize the number while respecting the constraint of no leading zeros
+        for (int i = 0; i < minLen; i++) {
+            char digit = minNum.charAt(i);
+            
+            // Case 1: Working with the first digit (most significant position)
             if (i == 0) {
                 if (digit != '1') {
-                    replace(min_num, digit, '1');
-                    break;
+                    // If the first digit is not already 1, replace it with 1
+                    // We use 1 instead of 0 to avoid leading zeros
+                    replace(minNum, digit, '1');
+                    break; // Only perform one replacement operation
                 }
-            } else {
-                if (digit != '0' && digit != min_num.charAt(0)) {
-                    replace(min_num, digit, '0');
-                    break;
+            } 
+            // Case 2: Working with non-first digits
+            else {
+                // For non-first positions, we want to replace with 0 to minimize the number
+                // BUT we need two checks:
+                // 1. Don't replace if the digit is already 0 (no change would occur)
+                // 2. Don't replace if the digit is the same as the first digit
+                //    This second check is crucial because if we replace a digit that also appears
+                //    as the first digit, we would accidentally create a leading zero
+                if (digit != '0' && digit != minNum.charAt(0)) {
+                    // Found a digit that can be safely replaced with 0
+                    replace(minNum, digit, '0');
+                    break; // Only perform one replacement operation
                 }
             }
         }
-
-        return (
-            Integer.parseInt(max_num.toString()) -
-            Integer.parseInt(min_num.toString())
-        );
+        
+        // Calculate and return the difference between the maximized and minimized numbers
+        return Integer.parseInt(maxNum.toString()) - Integer.parseInt(minNum.toString());
     }
 
+    /**
+     * Helper method to replace all occurrences of a character in a StringBuffer
+     * @param s The StringBuffer to modify
+     * @param x The character to be replaced
+     * @param y The character to replace with
+     */
     public void replace(StringBuffer s, char x, char y) {
-        int length = s.length();
-        for (int i = 0; i < length; ++i) {
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == x) {
                 s.setCharAt(i, y);
             }
