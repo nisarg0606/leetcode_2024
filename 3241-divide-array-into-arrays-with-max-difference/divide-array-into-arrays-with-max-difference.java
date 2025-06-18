d92260c6823f8a4d72b6384478f4e6af8dc7cb21
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-06-18 13:55:35 -0400

    Time: 24 ms (35.60%) | Memory: 62.2 MB (5.76%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -1,43 +1,14 @@
+import java.util.Arrays;
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
-        if (nums.length % 3 != 0) {
-            return new int[0][0];
-        }
-
-        int size = nums.length;
-
-        int max = 0;
-
-        for (int n : nums) {
-            max = Math.max(max, n);
-        }
-
-        int[] freqs = new int[max + 1];
-
-        for (int n : nums) {
-            ++freqs[n];
-        }
-
-        int[][] subs = new int[size / 3][3];
-
-        for (int n = 1, r = 0, c = 0; r < subs.length && n <= max;) {
-            if (freqs[n] == 0) {
-                ++n;
+        Arrays.sort(nums);
+        int[][] result = new int[nums.length/3][3];
+        for(int i = 2; i < nums.length; i += 3) {
+            if(nums[i] - nums[i-2] > k) {
+                return new int[0][];
             }
-            else if (c == subs[r].length) {
-                ++r;
-                c = 0;
-            }
-            else if (c == 0 || n - subs[r][0] <= k) {
-                subs[r][c] = n;
-                --freqs[n];
-                ++c;
-            }
-            else {
-                return new int[0][0];
-            }
+            result[i/3] = new int[] { nums[i-2], nums[i-1], nums[i] };
         }
-
-        return subs;
+        return result;
     }
 }
