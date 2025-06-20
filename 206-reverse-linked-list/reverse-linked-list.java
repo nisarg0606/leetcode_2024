be1dce42fbc33274851cce3a74f3df2c36e993c3
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-06-20 01:57:34 -0400

    Time: 0 ms (100.00%) | Memory: 42.5 MB (66.85%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,24 +10,20 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null)
-            return null;
-
-        Stack<Integer> stack = new Stack<>();
-
-        ListNode curr = head;
-        while (curr != null) {
-            stack.push(curr.val);
-            curr = curr.next;
+        ListNode temp = head, prev = null;
+        while(temp != null){
+            /*
+            Here we are taking prev node which is initally null as it will be the end of the list
+            then we are taking front pointer which points to the head's next whole part
+            now our head is pointing to the null in the first place
+            now we move our prev pointer to the head and the temp pointer to the head.next part so that we can move till the end and we will do this recursively till the head becomes null
+            in our case temp pointer
+            */
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
         }
-
-        ListNode newHead = new ListNode(stack.pop());
-        ListNode temp = newHead;
-        while (!stack.isEmpty()) {
-            temp.next = new ListNode(stack.pop());
-            temp = temp.next;
-        }
-
-        return newHead;
+        return prev;
     }
 }
