f9b810b19c845977a259da5fe95dcda4360d4b66
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-06-22 17:49:45 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (51.70%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
@@ -1,32 +1,12 @@
 class Solution {
     public String[] divideString(String s, int k, char fill) {
-        int n = s.length();
-        // Calculate the number of parts. Use Math.ceil equivalent for integer division.
-        // (n + k - 1) / k works for positive integers.
-        int numParts = (n + k - 1) / k;
+        StringBuilder sb = new StringBuilder(s);
+        while (sb.length() % k != 0) sb.append(fill);
+        int n = sb.length();
+        String[] res = new String[n / k];
+        for (int i = 0; i < n; i += k)
+            res[i / k] = sb.substring(i, i + k);
 
-        String[] result = new String[numParts];
-
-        for (int i = 0; i < numParts; i++) {
-            int startIndex = i * k;
-            // Determine the end index for the current substring
-            int endIndex = Math.min(startIndex + k, n);
-
-            // Extract the substring
-            String currentPart = s.substring(startIndex, endIndex);
-
-            // If it's the last part and it needs padding
-            if (currentPart.length() < k) {
-                StringBuilder sb = new StringBuilder(currentPart);
-                while (sb.length() < k) {
-                    sb.append(fill);
-                }
-                result[i] = sb.toString();
-            } else {
-                result[i] = currentPart;
-            }
-        }
-
-        return result;
+        return res;
     }
 }
