c394a87642268a6b2c5cd78c37f881cbcd5df365
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-07-13 17:16:34 -0400

    Time: 6 ms (72.37%) | Memory: 44.1 MB (86.57%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -23,16 +23,52 @@ class Solution {
 
         // return res;
 
-        int n = s.length(), ans = 0;
-        Map<Character, Integer> charToNextIndex = new HashMap<>();
-        for (int j = 0, i = 0; j < n; j++) {
-            if (charToNextIndex.containsKey((s.charAt(j)))) {
-                i = Math.max(charToNextIndex.get(s.charAt(j)), i);
-            }
-            ans = Math.max(ans, j - i + 1);
-            charToNextIndex.put(s.charAt(j), j + 1);
-        }
+        // int n = s.length(), ans = 0;
+        // Map<Character, Integer> charToNextIndex = new HashMap<>();
+        // for (int j = 0, i = 0; j < n; j++) {
+        //     if (charToNextIndex.containsKey((s.charAt(j)))) {
+        //         i = Math.max(charToNextIndex.get(s.charAt(j)), i);
+        //     }
+        //     ans = Math.max(ans, j - i + 1);
+        //     charToNextIndex.put(s.charAt(j), j + 1);
+        // }
 
-        return ans;
+        // return ans;
+
+        // Integer[] chars = new Integer[128];
+
+        // int left = 0;
+        // int right = 0;
+
+        // int res = 0;
+        // while(right < s.length()){
+        //     char r = s.charAt(right);
+
+        //     Integer index = chars[r];
+        //     if(index != null && index >= left && index < right){
+        //         left = index + 1;
+        //     }
+
+        //     res = Math.max(res, right - left + 1);
+
+        //     chars[r] = right;
+        //     right++;
+        // }
+
+        // return res;
+
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0, len = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (map.get(c) >= start)
+                    start = map.get(c) + 1;
+            }
+            len = Math.max(len, i - start + 1);
+            map.put(c, i);
+        }
+        return len;
     }
 }
