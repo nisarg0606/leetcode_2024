2a01ec5cd2d71f7fc2f635a76c35fdd32d61eff8
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-07-17 01:05:01 -0400

    Time: 7 ms (65.17%) | Memory: 47.8 MB (82.13%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,57 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        /*
-        For the brute force approach I think I will take a hashmap where I will first 
-        take the first word and create a char array out of it. Will sort the character
-        array and make it the key. Now when the new words arrive will check by sorting
-        the characters
-        Eg:
-        "eat" -> "aet"
-        "tea" -> "aet"
-        'tan' -> 'ant'
-        'nat' -> 'ant'
-        'bat' -> 'abt'
-        
-        "aet" -> ["eat", "tea", "ate"]
-        "ant" -> ["tan", "nat"]
-        "abt" -> ["bat"]
-        */
-
-        //    Map<String, List<String>> ans = new HashMap<>();
-
-        //    for(String s: strs){
-        //     char[] chars = s.toCharArray();
-        //     Arrays.sort(chars);
-        //     String key = new String(chars);
-        //     if(!ans.containsKey(key)){
-        //         ans.put(key, new ArrayList<>());
-        //     }
-        //     ans.get(key).add(s);
-        //    }
-
-        //    return new ArrayList<>(ans.values());
-
+        if (strs.length == 0)
+            return new ArrayList<>();
         Map<String, List<String>> ans = new HashMap<>();
-
         for (String s : strs) {
-            int[] count = new int[26];
-
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
-            }
-
-            StringBuilder sb = new StringBuilder();
-            for (int num : count) {
-                sb.append(num).append("#");
-            }
-            String key = sb.toString();
-            if (!ans.containsKey(key)) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key))
                 ans.put(key, new ArrayList<>());
-            }
             ans.get(key).add(s);
         }
 
         return new ArrayList<>(ans.values());
-
     }
+
 }
