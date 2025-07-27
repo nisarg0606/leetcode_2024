c8700d5829e4de64f1f48484b91e21fedebeaf73
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-07-27 17:46:38 -0400

    Time: 6 ms (23.30%) | Memory: 48.3 MB (99.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,47 +1,45 @@
-class Pair{
-	int row;
-	int col;
-	Pair(int row, int col){
-		this.row = row;
-		this.col = col;
-	}
+import java.util.*;
+
+class Pair {
+    int row;
+    int col;
+    Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
 }
 
 class Solution {
 
     public int numIslands(char[][] grid) {
-		int n = grid.length;
-		int m = grid[0].length;
-		int cntIslands = 0;
-		int[][] visited = new int[n][m];
-		Queue<Pair> queue = new LinkedList<>();
-		
-		int[] dRow = {-1, 0, 1, 0};
-		int[] dCol = {0, 1, 0, -1};
-		
-		for(int row = 0; row < n; row++){
-			for(int col = 0; col < m; col++){
-				if(grid[row][col] == '1' && visited[row][col] == 0){
-					cntIslands++;
-					visited[row][col] = 1;
-					queue.offer(new Pair(row, col));
-					while(!queue.isEmpty()){
-						int r = queue.peek().row;
-						int c = queue.peek().col;
-						queue.remove();
-						for(int i = 0; i < 4; i++){
-							int nRow = r + dRow[i];
-							int nCol = c + dCol[i];
-							if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && visited[nRow][nCol] == 0 && grid[nRow][nCol] == '1'){
-								visited[nRow][nCol] = 1;
-								queue.offer(new Pair(nRow, nCol));
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		return cntIslands;
-	}
+        int n = grid.length;
+        int m = grid[0].length;
+        int cntIslands = 0;
+        Queue<Pair> queue = new LinkedList<>();
+
+        int[][] directions = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (grid[row][col] == '1') {
+                    cntIslands++;
+                    grid[row][col] = '0'; // mark visited
+                    queue.offer(new Pair(row, col));
+                    while (!queue.isEmpty()) {
+                        Pair p = queue.poll();
+                        for (int[] dir : directions) {
+                            int newRow = p.row + dir[0];
+                            int newCol = p.col + dir[1];
+                            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == '1') {
+                                grid[newRow][newCol] = '0'; // mark visited
+                                queue.offer(new Pair(newRow, newCol));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return cntIslands;
+    }
 }
