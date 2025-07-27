95620d9c5525ab24987f90d76dc4c53f858b0cbc
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-07-27 16:59:19 -0400

    Time: 2 ms (67.68%) | Memory: 42.8 MB (29.51%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -12,51 +12,49 @@ class Pair {
 
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        int[][] visited = new int[n][m];
-        Queue<Pair> queue = new LinkedList<>();
-        int cntFresh = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    visited[i][j] = 2;
-                    queue.add(new Pair(i, j, 0));
-                } else {
-                    visited[i][j] = 0;
-                }
-
-                if (grid[i][j] == 1) {
-                    cntFresh++;
-                }
-            }
-        }
-
-        int time = 0;
-        int drow[] = { -1, 0, 1, 0 };
-        int dcol[] = { 0, 1, 0, -1 };
-        int cnt = 0;
-        while (!queue.isEmpty()) {
-            int r = queue.peek().row;
-            int c = queue.peek().col;
-            int t = queue.peek().time;
-            time = Math.max(time, t);
-            queue.remove();
-            for (int i = 0; i < 4; i++) {
-                int nRow = r + drow[i];
-                int nCol = c + dcol[i];
-                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && visited[nRow][nCol] == 0
-                        && grid[nRow][nCol] == 1) {
-                    queue.add(new Pair(nRow, nCol, t + 1));
-                    visited[nRow][nCol] = 2;
-                    cnt++;
-                }
-            }
-
-        }
-        if (cnt != cntFresh)
-            return -1;
-        return time;
-    }
+		int n = grid.length;
+		int m = grid[0].length;
+		
+		int[][] visited = new int[n][m];
+		Queue<Pair> queue = new LinkedList<>();
+		int cntFrest = 0;
+		
+		// Find the location of Rotten Oranges and create a visited array to mark them as rotten
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				if(grid[i][j] == 2){
+					visited[i][j] = 2;
+					queue.offer(new Pair(i, j, 0));
+				}else if(grid[i][j] == 1){
+					cntFrest++; // count number of frest oranges which can or cannot be rotten 
+				}else{
+					visited[i][j] = 0;
+				}
+			}
+		}
+		
+		int time = 0;
+		int[] dRow = { -1, 0, 1, 0 }; // up, right side, down, left side
+		int[] dCol = { 0, 1, 0, -1 }; // up, right side, down, left side
+		int rottenOranges = 0;
+		while(!queue.isEmpty()){
+			int r = queue.peek().row;
+			int c = queue.peek().col;
+			int t = queue.peek().time;
+			time = Math.max(t, time); // Update the time which it will take to rotten
+			queue.remove();
+			for(int i = 0; i < 4; i++){
+				int nRow = dRow[i] + r; // Check surrounding row
+				int nCol = dCol[i] + c; // Check Surrounding column
+				// Check if the row and column are in bound, they are not visted and oranges are fresh
+				if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == 1 && visited[nRow][nCol] == 0){
+					visited[nRow][nCol] = 2;
+					queue.offer(new Pair(nRow, nCol, t+1));
+					rottenOranges++;
+				}
+			}
+		}
+		if(rottenOranges == cntFrest)return time; // if all the oranges are rotten then return the time else return -1 as it failed to rotten all of them
+		return -1;
+	}
 }
