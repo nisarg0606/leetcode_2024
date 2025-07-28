94160d0992f628280c7ee5531afbd668ed764b16
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-07-28 14:26:33 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (43.04%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -16,16 +16,16 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        rightRideViewRec(root, result, 0);
+        rightView(root, result, 0);
         return result;
     }
 
-    public void rightRideViewRec(TreeNode current, List<Integer> result, int currDepth){
+    public void rightView(TreeNode current, List<Integer> result, int currDepth){
         if(current == null) return;
         if(currDepth == result.size()){
             result.add(current.val);
         }
-        rightRideViewRec(current.right, result, currDepth + 1);
-        rightRideViewRec(current.left, result, currDepth + 1);
+        rightView(current.right, result, currDepth + 1);
+        rightView(current.left, result, currDepth + 1);
     }
 }
