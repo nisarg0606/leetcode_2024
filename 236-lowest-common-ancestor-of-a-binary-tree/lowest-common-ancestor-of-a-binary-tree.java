cb189bd9f4daeb92424907e80434b8275639b405
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-07-28 22:59:29 -0400

    Time: 7 ms (60.17%) | Memory: 45 MB (26.20%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,36 +9,18 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Deque<TreeNode> stack = new ArrayDeque<>();
-
-        Map<TreeNode, TreeNode> parent = new HashMap<>();
-
-        parent.put(root, null);
-        stack.push(root);
-
-        while (!parent.containsKey(p) || !parent.containsKey(q)) {
-            TreeNode node = stack.pop();
-
-            if (node.left != null) {
-                parent.put(node.left, node);
-                stack.push(node.left);
-            }
-            if (node.right != null) {
-                parent.put(node.right, node);
-                stack.push(node.right);
-            }
+        if (root == null || root == p || root == q) {
+            return root;
         }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        Set<TreeNode> ancestors = new HashSet<>();
-
-        while (p != null) {
-            ancestors.add(p);
-            p = parent.get(p);
-        }
-
-        while (!ancestors.contains(q))
-            q = parent.get(q);
-
-        return q;
+        if (left == null)
+            return right;
+        else if (right == null)
+            return left;
+        else
+            return root;
     }
+
 }
