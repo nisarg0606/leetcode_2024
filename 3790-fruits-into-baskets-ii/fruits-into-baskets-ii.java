3dfa4551f6477d2470e2308133925e83beaac7d8
Nisarg Shah <64687773+nisarg0606@users.noreply.github.com>
2025-08-05 10:59:19 -0400

    Time: 2 ms (61.24%) | Memory: 44.5 MB (47.92%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,25 +1,35 @@
+// class Solution {
+//     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+//         // int count = 0;
+//         // for(int i = 0; i < fruits.length; i++){
+//         //     for(int j = 0; j < baskets.length; j++){
+//         //         if(fruits[i] <= baskets[j] && baskets[j] != -1){
+//         //             count++;
+//         //             baskets[j] = -1;
+//         //             break;
+//         //         }
+//         //     }
+//         // }
+//         // return baskets.length - count;
+//     }
+// }
+
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        int count = 0;
-        // int i = 0, j = 0;
-        // while(j < baskets.length && i < fruits.length){
-        //     if(fruits[i] <= baskets[j]){
-        //         count++;
-        //         i++;
-        //         j++;
-        //     }else{
-        //         j++;
-        //     }
-        // }
-        for(int i = 0; i < fruits.length; i++){
-            for(int j = 0; j < baskets.length; j++){
-                if(fruits[i] <= baskets[j] && baskets[j] != -1){
-                    count++;
-                    baskets[j] = -1;
+        int n = fruits.length;
+        boolean[] used = new boolean[n];
+        int placed = 0;
+
+        for (int i = 0; i < n && placed < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed++;
                     break;
                 }
             }
         }
-        return baskets.length - count;
+
+        return n - placed;
     }
 }
